b1e865b2b16e6516dd78d7c3cefdea93c9226909
Dhirendra Kumar Prajapati <officialprajapati0@gmail.com>
2022-12-20 04:30:07 +0530

    Added custom error handler to handle all types of error that can occur

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,5 +1,12 @@
+const { CustomError } = require("../errors/custom-error");
+
 const errorHandler = (err, req, res, next) => {
+  if (err instanceof CustomError)
+    return res.status(err.statusCode).json({ message: err.message });
   return res.status(500).json({ message: err });
+
+  // we can use this also for minimul error message
+  // return res.status(500).json({ message: `Something went wrong, please try again!` });
 };
 
 module.exports = errorHandler;
